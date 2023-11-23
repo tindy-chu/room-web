003338508dfa08ee0292cf9c1a8db0fdb94a68a7
Tindy <ljoink@yahoo.com>
2023-11-24 05:28:18 +0800

    update

--- public/env.js
+++
@@ -1,8 +0,0 @@
-window._env_ = {
-  BACKEND_URL: 'http://localhost:8000',
-  BACKEND_SSO_URI: '/oauth2/code',
-  VERSION: '1.0-local',
-  IAM_URL: 'https://lemur-11.cloud-iam.com',
-  IAM_REALM: 'tindy',
-  IAM_CLIENT_ID: 'public_client',
-};
--- src/hook/useAuth.tsx
+++ src/hook/useAuth.tsx
@@ -3,35 +3,15 @@ import { useEffect } from 'react';
 import str from '../utils/str';
 
 export default function useAuth() {
-  // const navigate = useNavigate();
   const isLogin = false;
+
   useEffect(() => {
     if (!isLogin) {
-      const iamUrl = str.getEnv('IAM_URL');
-      const realm = str.getEnv('IAM_REALM');
       const backendUrl = str.getEnv('BACKEND_URL');
-      const backendSsoUri = str.getEnv('BACKEND_SSO_URI');
-      const url = `${iamUrl}/realms/${realm}/protocol/openid-connect/auth`;
-      const redirectUri = `${backendUrl}${backendSsoUri}`;
+      const backendSsoUri = str.getEnv('BACKEND_LOGIN_URI');
+      const href = `${backendUrl}/${backendSsoUri}`;
 
-      // console.log(redirectUri);
-      const params = {
-        scope: 'openid',
-        response_type: 'code',
-        state: 'state_parameter_passthrough_value',
-        redirect_uri: redirectUri,
-        client_id: str.getEnv('IAM_CLIENT_ID'),
-      };
-
-      const href = `${url}?${new URLSearchParams(params)}`;
-      console.log(
-        `*****START 374e55 ${Math.random().toString(32)} href*****\n`,
-        JSON.stringify(href),
-        '\n***** END ****'
-      );
-      // window.location.href = `${url}?${new URLSearchParams(params)}`;
-
-      // navigate('/login');
+      window.location.href = href;
     }
   }, []);
 
